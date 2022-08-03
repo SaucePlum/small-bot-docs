6c43c1acadb73cec14ceb96f9a157f3a414cd305
ReadSmall <2660422452@qq.com>
2022-08-03 18:06:23 +0800

    feat: update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: 'vdoing', // 使用npm包主题
   title: "小念同学机器人官网",
   description: '小念同学机器人不仅可以帮助频道提升活跃, 更是能够带领频道走向巅峰的超级女侠!',
-  base: '/small-bot/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/small-bot-docs/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: false, // 代码行号
     extractHeaders: [ 'h2', 'h3', 'h4', 'h5', 'h6' ],
